0b0c8f90d20abcb262ef6203304b7c8bf1348afc
dudamaciej <maciekduda99@gmail.com>
2020-04-13 21:40:40 +0200

    first steps

---
+++ src/Main.tsx
@@ -0,0 +1,17 @@
+import React, { FC } from 'react';
+import { SafeAreaView, Platform, StatusBar } from 'react-native';
+import { NavigationContainer} from '@react-navigation/native';
+import BottomTabs from './components/BottomNavigation';
+
+
+interface IMainProps { }
+
+const Main: FC<IMainProps> = props => {
+    return (
+        <NavigationContainer>
+            <BottomTabs />
+        </NavigationContainer>
+    );
+};
+
+export default Main;
---
+++ src/components/BottomNavigation.tsx
@@ -0,0 +1,51 @@
+import React, {FC} from 'react';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { MaterialCommunityIcons } from 'react-native-vector-icons';
+import Profile from '../screens/profile'
+import ToDoList from '../screens/todolist';
+import Home from '../screens/home';
+
+
+const Tab = createBottomTabNavigator();
+const BottomTabs: FC = () => {
+  return (
+    <Tab.Navigator
+      initialRouteName="Home"
+      tabBarOptions={{
+        activeTintColor: '#e91e63',
+      }}
+    >
+      <Tab.Screen
+        name="Home"
+        component={Home}
+        options={{
+          tabBarLabel: 'Home',
+          tabBarIcon: ({ color, size }) => (
+            <MaterialCommunityIcons name="home" color={color} size={size} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="ToDoList"
+        component={ToDoList}
+        options={{
+          tabBarLabel: 'ToDoList',
+          tabBarIcon: ({ color, size }) => (
+            <MaterialCommunityIcons name="bell" color={color} size={size} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Profile"
+        component={Profile}
+        options={{
+          tabBarLabel: 'Profile',
+          tabBarIcon: ({ color, size }) => (
+            <MaterialCommunityIcons name="account" color={color} size={size} />
+          ),
+        }}
+      />
+    </Tab.Navigator>
+  );
+}
+export default BottomTabs;
